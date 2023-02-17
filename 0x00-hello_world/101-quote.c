9061bc2e56778e4b4bbb6bc93643dd223d49da79
T-redd01 <tyrilreddy1@gmail.com>
2023-02-16 23:15:40 -0800

    redo 3 for task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,11 @@
 
 /**
  * main - will print a string to stderr
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	write(1, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
