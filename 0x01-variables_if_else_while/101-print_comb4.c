c360cee206b57fff30b3d43f05b6f0d1f2c9384e
T-redd01 <tyrilreddy1@gmail.com>
2023-02-17 12:04:03 -0800

    rtask 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	int j;
+	int k;
+
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 10)
+		{
+			k = 0;
+			while (k < 10)
+			{
+				if (k != j && j != i && i < j && j < k)
+				{
+					putchar('0' + i);
+					putchar('0' + j);
+					putchar('0' + k);
+
+					if (k + j + i != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
