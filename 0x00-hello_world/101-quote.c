2caffbc478516b75bf98599806ecc0228da1246c
T-redd01 <tyrilreddy1@gmail.com>
2023-02-16 22:00:04 -0800

    c tasks redo

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - will print a string to stderr
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,14 +10,13 @@ int main(void)
 	char charVar;
 	int intVar;
 	long int longIntVar;
-	long long int longLongIntVar;
+	long long int long2IntVar;
 	float floatVar;
 
-	printf("Size of a char: %d byte(s)\n", sizeof(charVar));
-	printf("Size of a int: %d byte(s)\n", sizeof(intVar));
-	printf("Size of a long int: %d byte(s)\n", sizeof(longIntVar));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(longLongIntVar));
-	printf("Size of a float: %d byte(s)\n", sizeof(floatVar));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(charVar));
+	printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(intVar));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(longIntVar));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long2IntVar));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(floatVar));
 	return (0);
 }
-
