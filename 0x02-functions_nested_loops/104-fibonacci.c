04e3e17fdff7b9e502840ceee473ba794212c635
T-redd01 <tyrilreddy1@gmail.com>
2023-02-22 05:17:57 -0800

    task 16 redo.2

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,17 +7,30 @@
  */
 int main(void)
 {
-	long unsigned int i, j, k, n;
+	unsigned long int i, j, k, x, y, z, n;
 
 	j = 1;
 	k = 2;
-	for (i = 1; i <= 98; ++i)
+	printf("%lu", j);
+	for (i = 1; i < 91; i++)
 	{
-		printf("%lu, ", j);
-		n = j + k;
-		j = k;
-		k = n;
+		printf(", %lu", k);
+		k = k + j;
+		j = k - j;
 	}
+	x = j / 1000000000;
+	y = j % 1000000000;
+	z = k / 1000000000;
+	n = k % 1000000000;
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", z + (n / 1000000000));
+		printf("%lu", n % 1000000000);
+		z = z + x;
+		x = z - x;
+		n = n + y;
+		y = n - y;
+	}
+	printf("\n");
 	return (0);
 }
-
