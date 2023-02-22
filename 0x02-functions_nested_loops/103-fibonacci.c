97b949f91203477627d1bdd73b933cb5a85751b8
T-redd01 <tyrilreddy1@gmail.com>
2023-02-22 02:54:56 -0800

    task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long int i, j, k, n, x;
+
+	j = 1;
+	k = 2;
+	while (1)
+	{
+		if (k > 4000000)
+		{
+			break;
+		}
+		else if (k % 2 == 0)
+		{
+			x += k;
+		}
+		n = j + k;
+		j = k;
+		k = n;
+	}
+	printf("%ld\n", x);
+	return (0);
+}
