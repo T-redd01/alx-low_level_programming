abfb7b53a864673f41c4f21f6fc90467b7979d72
T-redd01 <tyrilreddy1@gmail.com>
2023-02-22 09:41:09 -0800

    task 3 redo.7

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,9 +2,10 @@
 #define MAIN_H
 
 #include <stdio.h>
+
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
-print_remaining_days();
+void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 
-#endif
+#endif /* MAIN_H */
