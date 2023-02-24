a9362c0c57cbf5c89a5be0cd9a1cd04668e22e19
T-redd01 <tyrilreddy1@gmail.com>
2023-02-24 06:42:24 -0800

    task 12 redo.1

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,26 +1,26 @@
 #include "main.h"
 
 /**
- * print_number - (recursive function) prints num using putchar
+ * print_number - prints using putchar
  * @n: num to print
  */
- void print_number(int n)
- {
-	 unsigned int x;
-	 
-	 if (n < 0)
-	 {
-		 x = -n;
-		 _putchar('-');
-	 }
-	 else
-	 {
-		 x = n;
-	 }
-	 
-	 if (x / 10)
-	 {
-		 print_number(x / 10);
-	 }
-	 _putchar((x % 10) + '0');
- }
+void print_number(int n)
+{
+	unsigned int x;
+
+	if (n < 0)
+	{
+		x = -n;
+		_putchar('-');
+	} else
+	{
+		x = n;
+	}
+
+	if (x / 10)
+	{
+		print_number(x / 10);
+	}
+
+	_putchar((x % 10) + '0');
+}
