478087daaad8597142a77a285f2a9138b9de436e
T-redd01 <tyrilreddy1@gmail.com>
2023-02-27 05:17:49 -0800

    task 4.

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_rev - print strin gin reverse
+ * @s: address of first char
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i-=1;
+
+	while (1)
+	{
+		if (!(i < 0))
+		{
+			_putchar(s[i]);
+			i--;
+		}
+		else
+		{
+			break;
+		}
+	}
+	_putchar('\n');
+}
+
