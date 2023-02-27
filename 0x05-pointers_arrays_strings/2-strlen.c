6d0367876c91542d7114d6499dc486e3a3fda543
T-redd01 <tyrilreddy1@gmail.com>
2023-02-27 02:50:28 -0800

    task 2.

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strlen - count number of chars in a string
+ * @s: pointer to 1st mem addr of string
+ *
+ * Return: num of chars
+ */
+int _strlen(char *s)
+{
+	int i = 1;
+
+	while(1)
+	{
+		if (*(s + 1) != '\0')
+		{
+			i++;
+			s++;
+		}
+		else
+		{
+			break;
+		}
+	}
+	return i;
+}
+
