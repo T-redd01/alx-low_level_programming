44af5bba833a44f6fe1f87de0344aa823b9491f2
T-redd01 <tyrilreddy1@gmail.com>
2023-02-28 01:59:32 -0800

    task 10. betty

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,53 +1,28 @@
 #include "main.h"
 
 /**
- * int _atoi(char *s) - return int of string
+ * _atoi - return int of string
  * @s: string to change
  *
  * Return: int num
  */
 int _atoi(char *s)
 {
-	int i, x, neg = 0, pt = 0, num = 0;
-	char dig, dig2;
+	int x = 1, i = 0;
+	unsigned int num = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
 	{
 		if (s[i] == '-')
-		{
-			neg++;
-		}
-		else if (pt != 1)
-		{
-			x = 0;
-			for (dig = '0'; dig <= '9'; dig++)
-			{
-				if (s[i] == dig)
-				{
-					for (dig2 = '0'; dig2 <= '9'; dig2++)
-					{
-						if (s[i + 1] == dig2)
-						{
-							pt = 0;
-							break;
-						}
-						else
-						{
-							pt = 1;
-						}
-					}
-					num = (num + x) * 10;
-				}
-				x++;
-			}
-		}
-		else
-			break;
+			x *= -1;
+		i++;
 	}
-	num /= 10;
-	for (i = 0; i < neg; i++)
+	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
 	{
-		num *= -1;
+		num = (num * 10) + (s[i] - '0');
+		i++;
 	}
+	num *= x;
 	return (num);
 }
+
