9681371452eb5072cb714811742cb88d97aa1bf8
T-redd01 <tyrilreddy1@gmail.com>
2023-03-02 06:19:46 -0800

    task 9. redo

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,16 +6,22 @@
  */
 void print_number(int n)
 {
+	unsigned int x;
+
 	if (n < 0)
 	{
 		n = -n;
 		_putchar('-');
 	}
-
-	if (n / 10)
+	else
 	{
-		print_number(n / 10);
+		x = n;
 	}
-	_putchar('0' + n % 10);
+
+	if (x / 10)
+	{
+		print_number(x / 10);
+	}
+	_putchar('0' + x % 10);
 }
 
