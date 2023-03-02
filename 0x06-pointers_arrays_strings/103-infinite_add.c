b6ce279735d65fb3f8c786f386e71410106f33fa
T-redd01 <tyrilreddy1@gmail.com>
2023-03-02 10:19:34 -0800

    task 11 redo

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,72 +1,78 @@
 #include "main.h"
 
 /**
- * add_digs - adds chars
- * @n1: addr
- * @n2: addr
- * @r: size
- * @r_index: index
- * Return: result
+ * rev_string - reverse string
+ * @n: integer params
+ * Return: 0
  */
-char *add_digs(char *n1, char *n2, char *r, int r_index)
+
+void rev_string(char *n)
 {
-	int num, tens = 0;
+	int i = 0;
+	int j = 0;
+	char temp;
 
-	for (; *n1 && *n2; n1--, n2-- r_index)
+	while (*(n + i) != '\0')
 	{
-		num = (*n1 - '0') + (*n2 - '0');
-		num += tens;
-		*(r + r_index) = (num % 10) + '0';
+		i++;
 	}
+	i--;
 
-	for (; *n1; n1--, r_index--)
+	for (j = 0; j < i; j++, i--)
 	{
-		num = (*n1 - '0') + tens;
-		*(r + r_index) = (num % 10) + '0';
-		tens = num / 10;
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
 	}
-
-	for (; *n2; n2--, r_index--)
-	{
-		num = (*n2 - '0') + tens;
-		*(r + r_index) = (num % 10) + '0';
-		tens = num / 10;
-	}
-
-	if (tens && r_index >= 0)
-	{
-		*(r + r_index) = (tens % 10) + '0';
-		return (r + r_index);
-	}
-	else if (tens && r_index < 0)
-		return (0);
-	return (r + r_index + 1);
 }
 
 /**
- * infinite_add - adds char arr
- * @n1: 1st arr
- * @n2: 2nd arr
- * @r: buff addr
- * @size_r: buff size
- * Return: 0 or r
+ * infinite_add - add 2 nums
+ * @n1: char arr
+ * @n2: char arr
+ * @r: addr of buff
+ * @size_r: size of buff
+ * Return: r or 0
  */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, n1_len = 0, n2_len = 0;
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
 
-	for (i = 0; *(n1 + i); i++)
-		n1_len++;
-
-	for (i = 0; *(n2 + i); i++)
-		n2_len++;
-
-	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
 		return (0);
-
-	n1 += n1_len - 1;
-	n2 += n2_len - 1;
-	*(r + size_r) = '\0';
-
-	return (add_digs(n1, n2, r, --size_r));
+	while (j >= 0 || i >= 0 || overflow == 1)
+	{
+		if (i < 0)
+			val1 = 0;
+		else
+			val1 = *(n1 + i) - '0';
+		if (j < 0)
+			val2 = 0;
+		else
+			val2 = *(n2 + j) - '0';
+		temp_tot = val1 + val2 + overflow;
+		if (temp_tot >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+		if (digits >= (size_r - 1))
+			return (0);
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
+		j--;
+		i--;
+	}
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
+	return (r);
 }
