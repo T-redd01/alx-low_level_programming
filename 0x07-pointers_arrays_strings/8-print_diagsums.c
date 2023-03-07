39b96aa6cafb2535fcbdbc04a072796ece69f0d7
T-redd01 <tyrilreddy1@gmail.com>
2023-03-06 22:27:28 -0800

    task 7 redo.6

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_diagsums - adds diagonal value of 2d square arr
@@ -9,12 +10,10 @@ void print_diagsums(int *a, int size)
 {
 	int i, x = 0, y = 0;
 
-	for (i = 0; i < (size * size); i++)
+	for (i = 0; i < size; i++)
 	{
-		if (i % (size + 1) == 0)
-			x += a[i];
-		if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
-			y += a[i];
+		x += *(a + (size * i + i));
+		y += *(a + (size * i + size - 1 - i));
 	}
 	printf("%d, %d\n", x, y);
 }
