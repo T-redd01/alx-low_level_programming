9fa628a8e35ff3fdfcf5b2e98c94d98f33bb4be1
T-redd01 <tyrilreddy1@gmail.com>
2023-03-16 05:30:17 -0700

    task 4 redo 2

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,7 +9,8 @@
  * @old_size: size of ptr
  * @new_size: bytes to reallocate
  * Return: newptr to new allocated mem
- */void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *newptr = NULL;
 
@@ -17,7 +18,11 @@
 		return (ptr);
 
 	if (ptr == NULL)
-		return (malloc(new_size));
+	{
+		newptr = malloc(new_size);
+		if (newptr == NULL)
+			return (NULL);
+	}
 
 	if (new_size == 0 && ptr != NULL)
 	{
