6e883aba202b26149497becc5938639f02c95544
T-redd01 <tyrilreddy1@gmail.com>
2023-03-16 05:41:02 -0700

    task 4 redo 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,7 +13,7 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	unsigned int i;
-	void *newptr = NULL;
+	char *newptr = NULL;
 
 	if (new_size == old_size)
 		return (ptr);
