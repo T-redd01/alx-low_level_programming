d3db960a36356e8d3e9700aa2d2fe441c007c058
T-redd01 <tyrilreddy1@gmail.com>
2023-03-23 12:26:23 -0700

    fix for betty

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -3,13 +3,13 @@
 /**
  * int_index - find index of match
  * @array: array to search
- * @size: size of arr 
+ * @size: size of arr
  * @cmp: func pointer
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
-	
+
 	if (size <= 0)
 		return (-1);
 
@@ -18,7 +18,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	for (i = 0; i < size; i++)
 	{
-		if(cmp(array[i]))
+		if (cmp(array[i]))
 			return (i);
 	}
 	return (-1);
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -6,27 +6,27 @@
  * main - Entry point
  * @argc: num of args
  * @argv: arr of args
- Return: 0 for Success
+ * Return: 0 for Success
  */
 int main(int argc, char *argv[])
 {
-    int (*op)(int, int);
+	int (*op)(int, int);
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    op = get_op_func(argv[2]);
+	op = get_op_func(argv[2]);
 
-    if (!op)
-    {
-        printf("Error\n");
-        exit(99);
-    }
+	if (!op)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-    printf("%d\n", op(atoi(argv[1]), atoi(argv[3])));
-    return (0);
+	printf("%d\n", op(atoi(argv[1]), atoi(argv[3])));
+	return (0);
 }
 
