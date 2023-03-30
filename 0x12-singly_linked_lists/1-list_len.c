bbb81e53e5a8f7cb231f62bcd08405b39cff779f
Tyril <tyrilreddy1@gmail.com>
2023-03-30 05:50:24 -0700

    task 1 err

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,5 +1,5 @@
 #include "lists.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * list_len - counts number of nodes in linked list
@@ -10,7 +10,7 @@ size_t list_len(const list_t *h)
 {
 	size_t len = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		len++;
 		h = h->next;
