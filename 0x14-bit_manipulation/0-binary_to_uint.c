34023efd5bf70d1c2dcb2ee49e62b6a7df42cab5
Tyril <tyrilreddy1@gmail.com>
2023-04-08 04:32:50 -0700

    task 0 redo

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,39 +1,33 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts binary to num
- * @b: addr of 1st char to conv
- * Return: 0 if fail num if succeed
+ * binary_to_uint - conv bin sring to num
+ * @b: addr of first char
+ * Return: 0 if b is null, 0 if char not 1 or 0, num on success
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0, idx = 0, pow2 = 1;
+	unsigned int num = 0, pow2 = 1, idx;
 
 	if (b == NULL)
 	{
 		return (0);
 	}
 
-	while (b[idx] != '\0')
-		idx++;
+	for (idx = 0; b[idx] != '\0'; idx++)
+	{
+		if (b[idx] > '1' || b[idx] < '0')
+			return (0);
+	}
 
-	idx -= 1;
 	while (idx)
 	{
-		if (b[idx] == '1')
-			num += pow2;
-		else if (b[idx] > '1' || b[idx] < '0')
-			return (0);
-		pow2 *= 2;
 		idx--;
-	}
-	if (b[idx] == '1')
-	{
-		num += pow2;
-	}
-	else if (b[idx] > '1' || b[idx] < '0')
-	{
-		return (0);
+		if (b[idx] == '1')
+		{
+			num += pow2;
+		}
+		pow2 *= 2;
 	}
 	return (num);
 }
