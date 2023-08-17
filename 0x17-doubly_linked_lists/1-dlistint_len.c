8b094aea5ce725ce8bae50f995964a996084a0a1
T-redd01 <tyrilreddy1@gmail.com>
2023-08-17 05:10:24 -0700

    trying

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -9,7 +9,7 @@
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t nodes = 0;
-	const dlistint_t *tmp = *h;
+	const dlistint_t *tmp = h;
 
 	if (!h)
 		return (0);
@@ -31,4 +31,3 @@ size_t dlistint_len(const dlistint_t *h)
 	}
 	return (nodes);
 }
-
---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - add node at beggining
+ * @head: list node
+ * @n: value of new node
+ *
+ * Return: new node (Success), NULL (Fail)
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistin_t *new, *tmp = *head;
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->prev = NULL;
+	new->next = NULL;
+
+	if (!(*head))
+	{
+		*head = new;
+		return (new);
+	}
+
+	while (tmp->prev)
+		tmp = tmp->prev;
+	tmp->prev = new;
+	new->next = prev;
+	return (new);
+}
