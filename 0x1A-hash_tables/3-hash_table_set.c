6226664a8119bc538bfbbb306bcd279239b14908
T-redd01 <tyrilreddy1@gmail.com>
2023-09-09 16:53:59 +0200

    trying

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,21 +15,23 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!key || !ht)
 		return (0);
 
-	node = malloc(sizeof(hash_node_t));
-	if (!node)
-		return (0);
-
 	index = key_index((const unsigned char *)key, ht->size);
 	tmp = ht->array[index];
 	while (tmp)
 	{
 		if (!(strcmp(key, tmp->key)))
 		{
+			free(tmp->value);
 			tmp->value = strdup(value);
 			return (1);
 		}
 		tmp = tmp->next;
 	}
+
+	node = malloc(sizeof(hash_node_t));
+	if (!node)
+		return (0);
+
 	node->key = strdup(key);
 	if (!(node->key))
 	{
@@ -39,6 +41,5 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	node->value = strdup(value);
 	node->next = ht->array[index];
 	ht->array[index] = node;
-
 	return (1);
 }
