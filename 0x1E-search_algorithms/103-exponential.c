a3dcfe3b9d28b93b515449c21a437b89a29e12cf
T-redd01 <tyrilreddy1@gmail.com>
2023-12-13 03:59:03 -0800

    trying

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,5 +1,8 @@
 #include "search_algos.h"
 
+void print_arr(int *arr, size_t beg, size_t end);
+int bin_search(int *array, size_t size, int value);
+
 /**
  * exponential_search - step i by squaring it
  * @array: arr to look in
@@ -22,7 +25,7 @@ int exponential_search(int *array, size_t size, int value)
 		if (array[i] > value)
 		{
 			printf("Value found between indexes [%lu] and [%lu]\n", l, i);
-			ret = binary_search(array + l, i - l, value);
+			ret = bin_search(array + l, i - l, value);
 			if (ret == -1)
 				return (-1);
 			return (ret + l);
@@ -31,8 +34,62 @@ int exponential_search(int *array, size_t size, int value)
 		l = i;
 		i *= 2;
 	}
-	ret = binary_search(array + l, size - l, value);
+	ret = bin_search(array + l, size - l, value);
 	if (ret == -1)
 		return (-1);
 	return (ret + l);
+}
+
+/**
+ * print_arr - print array that is being searched
+ * @arr: array of ints
+ * @beg: start index
+ * @end: end index
+ */
+void print_arr(int *arr, size_t beg, size_t end)
+{
+	printf("Searching in array:");
+	while (beg < end)
+	{
+		if ((beg + 1) == end)
+			printf(" %d\n", arr[beg]);
+		else
+			printf(" %d,", arr[beg]);
+		beg++;
+	}
+}
+
+/**
+ * bin_search - divide and conquer search algo
+ * @array: array of ints
+ * @size: size of array
+ * @value: value to look for
+ *
+ * Return: index of value in array, -1
+ */
+int bin_search(int *array, size_t size, int value)
+{
+	size_t i, mid = size / 2, beg = 0, end = size;
+
+	for (i = 0; i < size; i++)
+	{
+		print_arr(array, beg, end);
+		if (array[mid] == value)
+			return (mid);
+
+		if (mid == beg || mid == end)
+			break;
+
+		if (value > array[mid])
+		{
+			beg = mid;
+			mid = (mid + end) / 2;
+		}
+		else
+		{
+			end = mid;
+			mid = (beg + mid) / 2;
+		}
+	}
+	return (-1);
 }
